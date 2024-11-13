6ea7ad447198703c5d8e2dc1a6211090678f27f6
rogerzhuang <87434722+rogerzhuang@users.noreply.github.com>
2024-11-13 13:35:27 -0800

    switch to kafka-python

--- receiver/app.py
+++ receiver/app.py
@@ -6,7 +6,7 @@ import yaml
 import uuid
 import datetime
 import json
-from pykafka import KafkaClient
+from kafka import KafkaProducer
 
 with open('app_config.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -17,25 +17,24 @@ with open('log_config.yml', 'r') as f:
 
 logger = logging.getLogger('basicLogger')
 
-# Create a single KafkaClient instance
-kafka_client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
-topic = kafka_client.topics[str.encode(app_config['events']['topic'])]
-producer = topic.get_sync_producer()
+producer = KafkaProducer(
+    bootstrap_servers=f"{app_config['events']['hostname']}:{app_config['events']['port']}",
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
 
 def submit_air_quality_data(body):
     """ Forwards air quality data to Kafka """
     trace_id = str(uuid.uuid4())
     logger.info(f"Received event air quality request with a trace id of {trace_id}")
     
-    body['trace_id'] = trace_id  # Add trace_id to the body
+    body['trace_id'] = trace_id
     
     msg = {
         "type": "air_quality",
         "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
         "payload": body
     }
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
+    producer.send(app_config['events']['topic'], value=msg)
     
     logger.info(f"Returned event air quality response (Id: {trace_id}) with status 201")
     return NoContent, 201
@@ -45,15 +44,14 @@ def submit_weather_data(body):
     trace_id = str(uuid.uuid4())
     logger.info(f"Received event weather request with a trace id of {trace_id}")
     
-    body['trace_id'] = trace_id  # Add trace_id to the body
+    body['trace_id'] = trace_id
     
     msg = {
         "type": "weather",
         "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
         "payload": body
     }
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
+    producer.send(app_config['events']['topic'], value=msg)
     
     logger.info(f"Returned event weather response (Id: {trace_id}) with status 201")
     return NoContent, 201
